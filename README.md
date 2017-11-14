43a50764b8a7b68f7316d5a06498f62edd055aa5
Jacopo De Stefani <jacopo.de.stefani@gmail.com>
2017-11-14 15:55:38 +0100

    Implemented authentication and selective checkout. Updated README.

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # github-orgbatclone
-Python script to perform the batch clone of all the repositories related certain assignment in Github Classroom, from aGithub organization
+Python script to perform the batch clone of all the repositories related to a certain assignment in Github Classroom, from a given Github organization, eventually with a specific authentication.
+
+# How to use it
+```
+python batchDownloadScript.py -o organisation_name -a assignment_name -d "YYYY-MM-DD HH:HH" -u username 
+```
+
+Note:
+* ```organization_name``` is the only mandatory parameter, all the others are optionals.
+* Date must be supplied between quotes and with the specified format
+
 
 # Dependencies
-- Requests (>= 2.8.14)
-- Git
+* Subprocess
+* Requests (>= 2.8.14)
+* Git
--- batchDownloadScript.py
+++ batchDownloadScript.py
@@ -1,7 +1,10 @@
 import requests
 import sys
+import os
 from optparse import OptionParser
-from subprocess import call
+import subprocess
+import getpass
+import pdb
 
 
 if __name__ == "__main__":
@@ -13,26 +16,63 @@ if __name__ == "__main__":
                       dest="assignment_name",
                       default="",
                       help="Specify the assignment name on Github Classroom")
+   parser.add_option("-d", "--checkout_date",
+                      action="store",
+                      dest="checkout_date",
+                      default="",
+                      help='Specify the checkout date (format "YYYY-MM-DD HH:MM") to which the Git repositories should be moved')
    parser.add_option("-o", "--organisation",
                       action="store", 
                       dest="organisation_name",
                       default="",
                       help="Specify the organization name on Github")
+   parser.add_option("-u", "--username",
+                      action="store", 
+                      dest="username",
+                      default="",
+                      help="Specify the username on Github")
    (options, args) = parser.parse_args()
 
    # Force user to input both assignment and organisation name
-   if not(options.assignment_name) or not(options.organisation_name):
-      parser.error("Both organisation and assignment are mandatory parameters")
+   if not(options.organisation_name):
+      parser.error("Organisation name is a mandatory parameter")
 
-   print('[INFO] Organization:' + options.organisation_name + ' - Assignment:' + options.assignment_name)
+   print('[INFO] Organization: ' + options.organisation_name)
 
-   # Make HTTP request for JSON list of repositories
-   repo_list_request = requests.get('https://api.github.com/orgs/'+options.organisation_name+'/repos?per_page=200')
+   # If an assignment name is passed, print it
+   if options.assignment_name:
+      print('[INFO] Assignment: ' + options.assignment_name)
 
-   # Filter repositories according to assignment name and clone them
+   # If a username is passed, require authentication
+   if options.username:
+      print('Insert password for Github authentication for ' + options.username + ':')
+      password = getpass.getpass() 
+      # Make HTTP request for JSON list of repositories with credentials
+      repo_list_request = requests.get('https://api.github.com/orgs/'+options.organisation_name+'/repos?per_page=200',auth=(options.username,password))
+   else:
+      # Otherwise, make HTTP request for JSON list of repositories without
+      repo_list_request = requests.get('https://api.github.com/orgs/'+options.organisation_name+'/repos?per_page=200')
+
+   # Filter repositories according to assignment name (if present) and clone them
    for repository in repo_list_request.json():
-       if repository["name"].find(options.assignment_name):
-           call(["git", "clone", repository["clone_url"]])
-           
+      if options.assignment_name:
+         if repository["name"].find(options.assignment_name) != -1:
+            subprocess.call(["git", "clone", repository["clone_url"]])
+      else:
+         subprocess.call(["git", "clone", repository["clone_url"]])
+
+      # If a checkout date is set
+      if options.checkout_date:
+         curr_dir = os.getcwd() 
+         os.chdir(os.path.join(curr_dir,repository["name"])) # cd into the repository
+         commit_hash = subprocess.check_output(['git','rev-list', '-n', '1', '--before="'+options.checkout_date+'"', 'master']) # Find commit hash before desired dates
+         subprocess.call(['git','checkout', '-b', 'deadline', commit_hash[:-1].decode("UTF-8")]) # Create and checkout to a deadline branch given commit hash
+         os.chdir(curr_dir) #cd out of the repository
+
+
+
+
+
+
 
 
